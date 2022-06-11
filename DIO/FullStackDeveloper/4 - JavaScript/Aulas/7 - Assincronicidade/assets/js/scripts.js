486e82bf2e2acb978f670b7a0a9f46c6db0fbdff
Claudio <doisagosto@hotmail.com>
2022-06-10 23:06:33 -0300

    Projeto Jogo da Memoria

--- DIO/FullStackDeveloper/4 - JavaScript/Aulas/7 - Assincronicidade/assets/js/scripts.js
+++ DIO/FullStackDeveloper/4 - JavaScript/Aulas/7 - Assincronicidade/assets/js/scripts.js
@@ -1,18 +1,12 @@
 
-//const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
 const BASE_URL = 'https://randomfox.ca/floof/';
 
-
 const catBtn = document.getElementById('change-cat');
-
 
 const getCats = async () => {
     try {
         const data = await fetch(BASE_URL);
         const json = await data.json();
-
-        //return json.webpurl; //webpurl foi retirado da url acima(BASE_URL) utilizada acima para buscar as imagens
-
         return json.image; //image foi retirado da url acima(BASE_URL) utilizada acima para buscar as imagens
     } catch (e) {
         console.log(e.message);
